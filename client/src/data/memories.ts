fb77b7e1e369860da69e72b22aebc2413b9d8e83
amogh545 <amoghk136@gmail.com>
2025-07-11 14:31:49 +0530

    newpic&working_render

--- client/src/data/memories.ts
+++ client/src/data/memories.ts
@@ -12,49 +12,49 @@ export const memories: Memory[] = [
     title: "Our First Date 💕",
     description: "right then i knew this would go for long(oru naaya ennennekkumaayi) ,the feeling you'd understand me like no one else,u did and  that i could bully you ofc .i knew  i did the right thing taking you to that rooftop and asking you out .",
     date: "October 3, 2024",
-    image: "/patti.jpg"
+    image: "/first1date.jpg"
   },
   {
     id: 2,
     title: "movie date 🎬",
     description: "stree 2 day , i had fun not sure if it was cuz of the movie and then taking these cute pics with you ",
     date: "December 6th , 2024",
-    image: "https://webdev-6esb.onrender.com/client/public/firstdate.jpg"
+    image: "/firstdate.jpg"
   },
   {
     id: 3,
-    title: "second date 🐶",
+    title: "second month date 🐶",
     description: "i love taking care of my patti , look at u drinking from the bowl ",
     date: "November 3rd, 2024",
-    image: "https://webdev-6esb.onrender.com/client/public/patti.jpg"
+    image: "/patti.jpg"
   },
   {
     id: 4,
     title: "basement adventure 🌊",
     description: "our go to spot ,just us and the thrill of getting caught by someone but we do still have do whatever we want to . ",
     date: "anytime",
-    image: "https://github.com/Amoghkd/webdev/blob/main/client/public/basement.jpg?raw=true"
+    image: "/basement.jpg"
   },
  {
     id: 5,
     title: "coffe date (group study apprently)☕ ",
     description: "getting coffe and thinking we'll actually study ",
     date: "march,2025",
-    image: "https://webdev-6esb.onrender.com/client/public/starbucks.jpg"
+    image: "starbucks.jpg"
   },
   {
     id: 6,
     title: "cali date 🌴",
     description: "nothing needs to be said ",
     date: "all the time and today ",
-    image: "https://webdev-6esb.onrender.com/client/public/cali.jpg"
+    image: "cali.jpg"
   },
   {
     id: 7,
     title: "bus ride going back home 🚌",
     description: "watching you sleep on me just like that ,soo cute and adorable",
     date: "during colg  ",
-    image: "https://webdev-6esb.onrender.com/client/public/bus.jpg"
+    image: "bus.jpg"
   },
  
 ]; 
